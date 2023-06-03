d25e7056ce25e66bb22b0b42e06ef3212d05191f
laibaijiang <1319822288@163.com>
2023-06-03 14:21:27 +0800

    初始化项目

---
+++ src/main/java/com/lbj/panbackend/PanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lbj.panbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PanBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lbj/panbackend/PanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbj.panbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
